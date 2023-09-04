1ae86bdbd469330623bd159c2369e43ef2e6d752
vladifju <vlad.ifju@gmail.com>
2023-09-04 15:12:57 +0300

    BIG_update Created Game class. The main class for running, optimized in order to get the engine more modular for easier future implementations.

--- pygame- game/engine_init.py
+++ pygame- game/engine_init.py
@@ -1,11 +1,203 @@
 import pygame
 import random
 import math
-from load import gun_image, gun_image_rev, projectile_image,drop_image, enemy_images, player_image
-from user_settings import ENEMY_COL_RADIUS, PLAYER_COL_RADIUS, BULLET_SPEED, ENEMY_SPAWN_RATE
+import sys
+from user_settings import (
+    WINDOW_HEIGHT,
+    WINDOW_WIDTH,
+    START_MAX_HP,
+    START_MAX_XP,
+    DAMAGE_COOLDOWN,
+    PLAYER_COL_RADIUS,
+    SHOOT_DELAY,
+    BULLET_SPEED,
+    PLAYER_SPEED,
+    ENEMY_COL_RADIUS,
+    ENEMY_SPAWN_RATE,
+    FPS,
+)
+
+from load import (
+    player_image,
+    reset_image,
+    background_image,
+    shot_effect,
+    gun_image,
+    gun_image_rev,
+    projectile_image,
+    enemy_images,
+    drop_image,
+)
 
 enemies = []
 active_items = []
+
+
+class Game:
+    def __init__(self):
+        self.initialize_pygame()
+        self.setup_game()
+        self.setup_player()
+
+    def initialize_pygame(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+        pygame.display.set_caption("Legacy of Brok")
+        pygame.display.set_icon(player_image)
+        self.font = pygame.font.Font(None, 64)
+        self.clock = pygame.time.Clock()
+
+    def setup_game(self):
+        self.running = True
+        self.game_over = False
+        self.dt = 0
+        self.xp_bar = XPBar(max_xp=START_MAX_XP)
+        self.hp_bar = HPBar(max_hp=START_MAX_HP)
+        self.last_damage_time = 0
+        self.reset_button_rect = reset_image.get_rect()
+        self.reset_button_rect.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2 + 100)
+
+    def setup_player(self):
+        self.player = Player((WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2), xp_bar=self.xp_bar)
+
+    def reset_game(self):
+        self.game_over = False
+        self.reset_player()
+        self.reset_enemies()
+        self.reset_items()
+
+    def reset_player(self):
+        self.player.rect.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+        self.player.collected_items = []
+        self.player.level = 1
+        self.player.xp = 0
+
+    def reset_enemies(self):
+        Enemy.enemies.clear()
+        Enemy.active_explosions.clear()
+
+    def reset_items(self):
+        active_items.clear()
+        self.xp_bar.current_xp = 0
+        self.xp_bar.max_xp = START_MAX_XP
+        self.hp_bar.current_hp = START_MAX_HP
+
+
+#old version reset_game fun, for future implementation, execute order of instructions.
+    # def reset_game(self):
+    #     self.game_over = False
+    #     self.player.rect.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+    #     Enemy.enemies = []
+    #     Enemy.active_explosions = []
+    #     self.player.collected_items = []  # Reset collected items
+    #     self.player.level = 1  # Reset level
+    #     self.player.xp = 0
+    #     active_items.clear()
+    #     self.xp_bar.current_xp = 0
+    #     self.xp_bar.max_xp = 10
+    #     self.hp_bar.current_hp = 100
+    #     self.player.rect.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+
+#GAME MAIN LOOP
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+
+            mouse_pos = pygame.mouse.get_pos()
+
+            if not self.game_over:
+                self.screen.blit(background_image, (0, 0))  # DRAW BACKGROUND
+                keys = pygame.key.get_pressed()
+                self.player.update(self.dt, keys, mouse_pos=mouse_pos)
+                self.player.draw(self.screen)
+
+                # Update enemy movement
+                for enemy in Enemy.enemies[:]:
+                    enemy.update(self.dt, self.player)
+
+                # Detect and handle bullet-enemy collisions + explosions
+                for bullet in self.player.projectiles[:]:
+                    bullet.update(self.dt)
+                    for enemy in Enemy.enemies[:]:
+                        if bullet.rect.colliderect(enemy.rect):
+                            self.player.projectiles.remove(bullet)
+                            Enemy.enemies.remove(enemy)
+                            enemy.kill()
+                            explosion = Explosion(shot_effect, enemy.rect.center, 200)  # 200 = ms
+                            Enemy.active_explosions.append(explosion)
+                            break
+                for explosion in Enemy.active_explosions[:]:
+                    if explosion.update():
+                        Enemy.active_explosions.remove(explosion)
+                    else:
+                        explosion.draw(self.screen)
+
+                # Remove bullets and enemies when off-screen
+                for bullet in self.player.projectiles[:]:
+                    if (
+                        bullet.rect.left > self.screen.get_width()
+                        or bullet.rect.right < 0
+                        or bullet.rect.top > self.screen.get_height()
+                        or bullet.rect.bottom < 0
+                    ):
+                        self.player.projectiles.remove(bullet)
+
+                # Colision test enemies
+                for enemy in Enemy.enemies[:]:
+                    if enemy.check_collision(self.player):
+                        current_time = pygame.time.get_ticks()
+                        if current_time - self.last_damage_time > DAMAGE_COOLDOWN:
+                            self.hp_bar.update(10)
+                            self.last_damage_time = current_time
+                            if self.hp_bar.current_hp <= 0:
+                                self.game_over = True
+                        break
+
+                # enemy spawner.
+                Enemy.spawn_random_enemy(WINDOW_WIDTH, WINDOW_HEIGHT)
+
+                for bullet in self.player.projectiles:
+                    bullet.update(self.dt)
+                    rotated_bullet = pygame.transform.rotate(bullet.image, -bullet.angle)  # Rotate bullet image
+                    rotated_rect = rotated_bullet.get_rect(center=bullet.rect.center)
+                    self.screen.blit(rotated_bullet, rotated_rect.topleft)
+
+                firing_point = self.player.rect.center + self.player.gun_offset.rotate(-self.player.gun_angle)
+
+                for enemy in Enemy.enemies:
+                    enemy.draw(self.screen)
+
+                for item in active_items[:]:
+                    if isinstance(item, Crystal):
+                        item.draw(self.screen)
+                    item.draw(self.screen)
+
+                self.xp_bar.draw(self.screen)
+                self.hp_bar.draw(self.screen)
+
+            else:
+                game_over_text = self.font.render("Game Over", True, (255, 0, 0))
+                text_rect = game_over_text.get_rect(
+                    center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+                )
+                self.screen.blit(game_over_text, text_rect)
+                self.screen.blit(reset_image, self.reset_button_rect)
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                mouse_clicked = pygame.mouse.get_pressed()
+
+                if (
+                    self.reset_button_rect.collidepoint(mouse_x, mouse_y)
+                    and mouse_clicked[0]
+                ):
+                    self.reset_game()
+
+            pygame.display.flip()
+            self.dt = self.clock.tick(FPS) / 1000  # limits FPS to 60
+
+        pygame.quit()
+        sys.exit()
 
 class Explosion:
     def __init__(self, image, position, duration):
@@ -53,7 +245,7 @@ class Player:
         self.gun_angle = 0
         self.projectiles = []
         self.shoot_cooldown = 0
-        self.shoot_delay = 0.35
+        self.shoot_delay = SHOOT_DELAY
         self.gun_direction = pygame.Vector2(1, 0)
         self.xp = 0
         self.level = 1
@@ -72,13 +264,13 @@ class Player:
 
     def update(self, dt, keys, mouse_pos):
         if keys[pygame.K_w] and self.rect.top > self.window_bounds.top:
-            self.rect.y -= 300 * dt
+            self.rect.y -= PLAYER_SPEED * dt
         if keys[pygame.K_s] and self.rect.bottom < self.window_bounds.bottom:
-            self.rect.y += 300 * dt
+            self.rect.y += PLAYER_SPEED * dt
         if keys[pygame.K_a] and self.rect.left > self.window_bounds.left:
-            self.rect.x -= 300 * dt
+            self.rect.x -= PLAYER_SPEED * dt
         if keys[pygame.K_d] and self.rect.right < self.window_bounds.right:
-            self.rect.x += 300 * dt
+            self.rect.x += PLAYER_SPEED * dt
 
         self.gun_rect.center = self.rect.center + self.gun_offset
         angle = math.atan2(mouse_pos[1] - self.rect.centery, mouse_pos[0] - self.rect.centerx)
@@ -120,7 +312,7 @@ class Enemy:
 
     enemies = []
     active_explosions = []
-    
+
     def __init__(self, image, spawn_point):
         self.image = image
         self.rect = image.get_rect(center=spawn_point)
